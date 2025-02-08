f15dd2a9e78042033cd768bc2b61d16f4c817b8f
DenisK111 <denislavkraev@gmail.com>
2025-02-08 10:09:49 +0200

    LongestCommonPrefix

---
+++ Leetcode/ConsoleApp1/Problems/Easy/LogestCommonPrefix.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeetcode.Problems.Easy
+{
+    internal class LogestCommonPrefix
+    {
+        public string LongestCommonPrefixSolve(string[] strs)
+        {
+            var sb = new StringBuilder();
+
+            int minLength = -1;
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                minLength = minLength == -1 ? strs[i].Length : Math.Min(minLength, strs[i].Length);
+            }
+
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (strs.All(str => str[i] == strs[0][i]))
+                {
+                    sb.Append(strs[0][i]);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            return sb.ToString();
+        }
+
+        [TestCase(new string[] { "flower", "flow", "flight" }, "fl")]
+        [TestCase(new string[] { "dog", "racecar", "car" }, "")]
+        public void Test(string[] input, string expected) => LongestCommonPrefixSolve(input).ShouldBe(expected);        
+    }
+}
