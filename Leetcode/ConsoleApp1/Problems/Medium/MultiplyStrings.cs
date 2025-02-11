1fe0bf5522f1462026c3401a1da558053333d342
DenisK111 <denislavkraev@gmail.com>
2025-02-11 08:25:59 +0200

    43. Multiply

---
+++ Leetcode/ConsoleApp1/Problems/Medium/MultiplyStrings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leeetcode.Problems.Medium
+{
+    /// <summary>
+    /// 43
+    /// </summary>
+    internal class MultiplyStrings
+    {
+        public string Multiply(string num1, string num2)
+        {
+            if (num1 == "0" || num2 == "0") return "0";            
+            int[] result = new int[num1.Length + num2.Length];
+
+            for (int i = num1.Length - 1; i >= 0; i--)
+            {
+                for (int j = num2.Length - 1; j >= 0; j--)
+                {
+                    int mul = (num1[i] - '0') * (num2[j] - '0');
+                    int sum = mul + result[i + j + 1]; 
+
+                    result[i + j + 1] = sum % 10;  
+                    result[i + j] += sum / 10;    
+                }
+            }
+
+            return string.Join("", result.SkipWhile(x => x == 0));
+        }
+    }
+}
