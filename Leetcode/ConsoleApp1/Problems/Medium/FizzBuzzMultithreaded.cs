1ce8aa8cbb559d28769c7714e1aa8ed4bc379c2e
DenisK111 <denislavkraev@gmail.com>
2025-03-05 15:15:40 +0200

    1195. Fizz Buzz Multithreaded

---
+++ Leetcode/ConsoleApp1/Problems/Medium/FizzBuzzMultithreaded.cs
@@ -0,0 +1,154 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using static Leeetcode.Problems.Medium.FizzBuzz;
+
+
+namespace Leeetcode.Problems.Medium;
+
+/// <summary>
+/// 1195
+/// </summary>
+
+public class FizzBuzz
+{
+    public const string FIZZ = "fizz";
+    public const string BUZZ = "buzz";
+    public const string FIZZBUZZ = "fizzbuzz";
+    public const string NUMBER = "number";
+
+    private readonly Dictionary<string, SemaphoreSlim> _sempahoreRegistry = new()
+    {
+        [FIZZ] = new(0),
+        [BUZZ] = new(0),
+        [FIZZBUZZ] = new(0),
+        [NUMBER] = new(1),
+    };
+
+    private int n;
+    private int number = 1;
+    public bool InRange => number <= n;
+    public FizzBuzz(int n)
+    {
+        this.n = n;
+    }
+
+    // printFizz() outputs "fizz".
+    public void Fizz(Action printFizz) => RunInLoop(() => Process(FIZZ, printFizz));
+
+    // printBuzzz() outputs "buzz".
+    public void Buzz(Action printBuzz) => RunInLoop(() => Process(BUZZ, printBuzz));
+
+    // printFizzBuzz() outputs "fizzbuzz".
+    public void Fizzbuzz(Action printFizzBuzz) => RunInLoop(() => Process(FIZZBUZZ, printFizzBuzz));
+
+    // printNumber(x) outputs "x", where x is an integer.
+    public void Number(Action<int> printNumber) => RunInLoop(() =>
+    {
+        Wait(NUMBER);
+        var _ = number switch
+        {
+            var x when number % 3 == 0 && number % 5 == 0 => Release(FIZZBUZZ),
+            var x when number % 3 == 0 => Release(FIZZ),
+            var x when number % 5 == 0 => Release(BUZZ),
+            _ => ExecuteOrReleaseAll(() => printNumber(number))
+        };
+    }).Then(ReleaseAll);
+
+    private void ReleaseAll()
+    {
+        foreach (var semaphore in _sempahoreRegistry.Values) semaphore.Release();
+    }
+
+    private Unit RunInLoop(Action action)
+    {
+        while (InRange) action();
+        return Unit.Default;
+    }
+
+    private ExecutionResult ExecuteIf(bool condition, Action action)
+    {
+        if (condition)
+        {
+            action();
+            return ExecutionResult.Success();
+        }
+        return ExecutionResult.Failure();
+    }
+
+    private Unit ExecuteOrReleaseAll(Action action) => ExecuteIf(InRange, () =>
+    {
+        action();
+        number++;
+        Release(NUMBER);
+    }).Else(ReleaseAll);
+
+    private Unit Wait(string key)
+    {
+        _sempahoreRegistry[key].Wait();
+        return Unit.Default;
+    }
+
+    private Unit Release(string key)
+    {
+        _sempahoreRegistry[key].Release();
+        return Unit.Default;
+    }
+    private void Process(string key, Action action)
+    {
+        Wait(key);
+        ExecuteOrReleaseAll(action);
+    }
+
+    public record ExecutionResult
+    {
+        public required bool IsSuccess { get; init; }
+        private ExecutionResult() { }
+        public static ExecutionResult Success() => new ExecutionResult { IsSuccess = true };
+        public static ExecutionResult Failure() => new ExecutionResult { IsSuccess = false };
+
+        public Unit Else(Action action)
+        {
+            if (!IsSuccess)
+                action();
+            return Unit.Default;
+        }
+    };
+
+    public record struct Unit
+    {
+        public static Unit Default => new();
+    }
+
+}
+
+public static class Extensions
+{
+    public static Unit Then(this Unit unit, Action action)
+    {
+        action();
+        return unit;
+    }
+}
+
+public class Tests
+{
+    [TestCase(7, new object[] { 1, 2, "fizz", 4, "buzz", "fizz", 7 })]
+    [TestCase(1, new object[] { 1 })]
+    [TestCase(15, new object[] { 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" })]
+    public void Test(int n, object[] expectedResult)
+    {
+        var resultList = new List<object>();
+        var fizzBuzz = new FizzBuzz(n);
+        var threads = new List<Thread>()
+        {
+            new Thread(() => fizzBuzz.Fizz(() => resultList.Add(FIZZ))),
+            new Thread(() => fizzBuzz.Buzz(() => resultList.Add(BUZZ))),
+            new Thread(() => fizzBuzz.Fizzbuzz(() => resultList.Add(FIZZBUZZ))),
+            new Thread(() => fizzBuzz.Number((i) => resultList.Add(i)))
+        };
+
+        threads.ForEach(t => t.Start());
+        threads.ForEach(t => t.Join());
+        resultList.ShouldBeEquivalentTo(expectedResult.ToList());
+    }
+}
---
+++ Leetcode/ConsoleApp1/Problems/Medium/PrintZeroEvenOdd.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Leeetcode.Problems.Medium
+{
+    /// <summary>
+    /// 1116
+    /// </summary>
+    public class ZeroEvenOdd
+    {
+        private int n;         
+        private SemaphoreSlim semaphoreZero = new(1);
+        private SemaphoreSlim semaphoreEven = new(0);
+        private SemaphoreSlim semaphoreOdd = new(0);
+
+        public ZeroEvenOdd() { }
+
+        public ZeroEvenOdd(int n)
+        {
+            this.n = n;
+        }     
+
+        // printNumber(x) outputs "x", where x is an integer.
+        public void Zero(Action<int> printNumber)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                semaphoreZero.Wait();
+                printNumber(0);
+                if (i % 2 != 0)
+                {
+                    semaphoreOdd.Release();
+                }
+                else
+                {
+                    semaphoreEven.Release();
+                }
+
+            }
+        }
+
+        public void Even(Action<int> printNumber)
+        {
+            for(int i = 2;i <= n; i+=2)
+            {
+                semaphoreEven.Wait();
+                printNumber(i);
+                semaphoreZero.Release();
+            }
+        }
+
+        public void Odd(Action<int> printNumber)
+        {
+            for (int i = 1; i <= n; i += 2)
+            {
+                semaphoreOdd.Wait();
+                printNumber(i);
+                semaphoreZero.Release();
+            }
+        }     
+        
+    }
+
+
+}
